da5189c4c2f066fd903da67137a07bc38f6f7f73
Sławomir Kowalski <suawekk@gmail.com>
2017-03-27 16:41:53 +0200

    more updates for shell wrapper

--- shell_wrapper.sh
+++ shell_wrapper.sh
@@ -3,9 +3,11 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR
 SCRIPT="./sentinel-notify-slack.rb"
 CONFIG="config.yml"
 
+TYPE=$1
+TEXT="$2"
+
 # use noninteractive login shell to ensure rbenv/rvm works
-bash -lc "$SCRIPT -c $CONFIG -- \"$@\""
+bash -lc -- "cd $DIR && $SCRIPT -c $CONFIG -- $TYPE \"$TEXT\""
